52e47ad584f67889c15cc38f3b035b626dab8fb6
Grace <202101186@iacademy.edu.ph>
2023-06-30 09:45:49 +0800

    a

---
+++ app/src/main/java/com/example/softeng2/sign.kt
@@ -0,0 +1,10 @@
+package com.example.softeng2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class sign : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
